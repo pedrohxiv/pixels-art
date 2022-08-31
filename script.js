eba0c77055e738f76a9b0faebec1d6694fbe9502
Pedro <pedrohalmeidamendonca@gmail.com>
2022-08-31 20:12:30 -0300

    adicionando uma função para selecionar uma cor na paleta de cores e preencha os pixels no quadro

--- script.js
+++ script.js
@@ -14,6 +14,39 @@ function randomColor() {
   }, ${Math.random() * 255}, ${Math.random() * 255})`));
 }
 
-document
-  .getElementById('button-random-color')
-  .addEventListener('click', randomColor);
+function selectFirstPaletteColor() {
+    document.querySelector('.first-block').classList.remove('selected');
+    document.querySelector('.second-block').classList.remove('selected');
+    document.querySelector('.third-block').classList.remove('selected');
+    document.querySelector('.fourth-block').classList.remove('selected');
+    document.querySelector('.first-block').classList.add('selected');
+}
+
+function selectSecondPaletteColor() {
+    document.querySelector('.first-block').classList.remove('selected');
+    document.querySelector('.second-block').classList.remove('selected');
+    document.querySelector('.third-block').classList.remove('selected');
+    document.querySelector('.fourth-block').classList.remove('selected');
+    document.querySelector('.second-block').classList.add('selected');
+}
+
+function selectThirdPaletteColor() {
+    document.querySelector('.first-block').classList.remove('selected');
+    document.querySelector('.second-block').classList.remove('selected');
+    document.querySelector('.third-block').classList.remove('selected');
+    document.querySelector('.fourth-block').classList.remove('selected');
+    document.querySelector('.third-block').classList.add('selected');
+}
+function selectFourthPaletteColor() {
+    document.querySelector('.first-block').classList.remove('selected');
+    document.querySelector('.second-block').classList.remove('selected');
+    document.querySelector('.third-block').classList.remove('selected');
+    document.querySelector('.fourth-block').classList.remove('selected');
+    document.querySelector('.fourth-block').classList.add('selected');
+}
+
+document.getElementById('button-random-color').addEventListener('click', randomColor);
+document.querySelector('.first-block').addEventListener('click', selectFirstPaletteColor);
+document.querySelector('.second-block').addEventListener('click', selectSecondPaletteColor);
+document.querySelector('.third-block').addEventListener('click', selectThirdPaletteColor);
+document.querySelector('.fourth-block').addEventListener('click', selectFourthPaletteColor);
