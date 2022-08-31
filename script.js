825fcd840b0e647659374a5a4d3e93f30d2b1d63
Pedro <pedrohalmeidamendonca@gmail.com>
2022-08-31 19:05:53 -0300

    adicionando uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+document.querySelector('.second-block').style.backgroundColor = JSON.parse(localStorage.getItem('colorPalette'));
+document.querySelector('.third-block').style.backgroundColor = JSON.parse(localStorage.getItem('colorPalette2'));
+document.querySelector('.fourth-block').style.backgroundColor = JSON.parse(localStorage.getItem('colorPalette3'));
+
 function randomColor() {
-  document.querySelector('.second-block').style.backgroundColor = `rgb(${
+  localStorage.setItem('colorPalette', JSON.stringify(document.querySelector('.second-block').style.backgroundColor = `rgb(${
     Math.random() * 255
-  }, ${Math.random() * 255}, ${Math.random() * 255})`;
-  document.querySelector('.third-block').style.backgroundColor = `rgb(${
+  }, ${Math.random() * 255}, ${Math.random() * 255})`));
+  localStorage.setItem('colorPalette2', JSON.stringify(document.querySelector('.third-block').style.backgroundColor = `rgb(${
     Math.random() * 255
-  }, ${Math.random() * 255}, ${Math.random() * 255})`;
-  document.querySelector('.fourth-block').style.backgroundColor = `rgb(${
+  }, ${Math.random() * 255}, ${Math.random() * 255})`));
+  localStorage.setItem('colorPalette3', JSON.stringify(document.querySelector('.fourth-block').style.backgroundColor = `rgb(${
     Math.random() * 255
-  }, ${Math.random() * 255}, ${Math.random() * 255})`;
+  }, ${Math.random() * 255}, ${Math.random() * 255})`));
 }
 
 document
